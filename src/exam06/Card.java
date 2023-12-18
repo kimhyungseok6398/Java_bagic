5f7d8b5424832098522d6772417ce9d6bf839675
kimhyungseok6398 <gudtjr6398@naver.com>
2023-12-19 00:02:58 +0900

    클래스 변수와 인스턴스 변수 복습

---
+++ src/exam06/Card.java
@@ -0,0 +1,9 @@
+package exam06;
+
+public class Card {
+    String kind;
+    int number;
+    static int width = 100;
+    static int height = 200;
+}
+
---
+++ src/exam06/Ex06_3.java
@@ -0,0 +1,31 @@
+package exam06;
+
+public class Ex06_3 {
+        public static void main(String[] args) {
+            System.out.println("card.width = " + Card.width);
+            System.out.println("card.height = " + Card.height);
+
+            Card c1 = new Card();
+            c1.kind = "Heart";
+            c1.number = 7;
+
+            Card c2 = new Card();
+            c2.kind = "Spade";
+            c2.number = 4;
+
+            System.out.println("c1은 " + c1.kind + ", " + c1.number + "이며 , 크기는 (" + c1.width + "," + c1.height + ")");
+            System.out.println("c2은 " + c2.kind + ", " + c2.number + "이며 , 크기는 (" + c2.width + "," + c2.height + ")");
+
+            System.out.println("c1을 바꾸겠습니다!");
+
+            c1.width = 50;
+            c1.height = 80;
+
+            System.out.println("c1은 " + c1.kind + ", " + c1.number + "이며 , 크기는 (" + c1.width + "," + c1.height + ")");
+            System.out.println("c2은 " + c2.kind + ", " + c2.number + "이며 , 크기는 (" + c2.width + "," + c2.height + ")");
+
+        }
+    }
+
+
+
